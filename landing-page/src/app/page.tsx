3ae78c083e83f5129cbb0258a937eca2466e69e2
Neema <neemamwende009@gmail.com>
2024-11-05 19:25:43 +0300

    Add Services component

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Services from '../components/Services';
 
 export default function HomePage() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Services />
       {/* Other components will go here */}
     </main>
   );
---
+++ landing-page/src/components/Services.tsx
@@ -0,0 +1,13 @@
+// components/Services.tsx
+import React from 'react';
+
+const Services: React.FC = () => {
+  return (
+    <section>
+      <h2>Our Services</h2>
+      <p>We provide the best solutions for your needs.</p>
+    </section>
+  );
+};
+
+export default Services;
