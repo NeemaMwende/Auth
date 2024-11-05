859a8fd77259120e20cd2e117c9caa001b98dc27
Neema <neemamwende009@gmail.com>
2024-11-05 19:22:48 +0300

    Added Hero component

---
+++ landing-page/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+// components/Hero.tsx
+import React from 'react';
+
+const Hero: React.FC = () => {
+  return (
+    <section>
+      <h1>Welcome to Our Landing Page</h1>
+      <p>Your success journey starts here.</p>
+    </section>
+  );
+};
+
+export default Hero;
