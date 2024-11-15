1904058dc33b09add43a685319a3fd47f02ffafb
Neema <neemamwende009@gmail.com>
2024-11-15 13:03:36 +0300

    heart design and animation

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,16 +1,15 @@
-// app/page.tsx
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
+// pages/index.js
+import HeartCanvas from '../components/Heart';
+import BubbleText from "../components/BubbleText";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Services />
-      {/* Other components will go here */}
-    </main>
+    <div className="relative min-h-screen flex items-center justify-center">
+      <HeartCanvas />
+      <div className="z-10 text-white text-center">
+        {/* <h1 className="text-4xl font-bold mb-4">Mi Curacacha</h1> */}
+        <BubbleText text="Mi Curacacha" /> 
+      </div>
+    </div>
   );
 }
